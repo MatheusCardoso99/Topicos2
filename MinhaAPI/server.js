9553711ffd32c517263e9b192b8468bf6c42d13d
MatheusCardoso99 <86811388+MatheusCardoso99@users.noreply.github.com>
2022-08-22 20:40:31 -0300

    api

--- MinhaAPI/server.js
+++ MinhaAPI/server.js
@@ -3,7 +3,11 @@ console.log("Servidor inicializando...");
 // Usando o Express
 const express = require('express');
 const app = express();
-app.use(express.json());//Para tratar o json
+app.use(express.json()); //Para tratar o json
+
+app.listen(3000, () =>{
+    console.log('Rodando em http://localhosta.3000')   
+})
 
 // prerarar para responder ao GET
 app.get('/', (req, res) => {
